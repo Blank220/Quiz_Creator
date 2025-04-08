9f3420760761af9b5a0420bebccb3de3b9e5998c
Mark Comia <marklestercomia22@gmail.com>
2025-04-08 18:48:25 +0800

    infinitely ask the user for questions and possible answers ranging from option a to d

---
+++ quiz_creator.py
@@ -0,0 +1,7 @@
+while True:
+    question = input('Enter A Question Of Your Choice: ')
+    print('Now, What Are The Possible Answers? ')
+    option_a = input('Option A. ')
+    option_b = input('Option B. ')
+    option_c = input('Option C. ')
+    option_d = input('Option D. ')
