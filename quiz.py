039453787c418a9595dbdc46da2e2d0861c89109
Mark Comia <itzextinct2@gmail.com>
2025-05-05 10:36:04 +0800

    added pseudocodes to follow

--- quiz.py
+++ quiz.py
@@ -1,9 +1,15 @@
 import random
 
+#open the quiz file
 with open('quiz.txt', 'r') as file:
     lines = file.readlines()
 
+#formatting of the questions/
 questions = []
 i = 0
 while i < len(lines):
-    
+
+#choosing random questions
+#printing the question and choices
+#checks if answer is right
+#tracking of scores
