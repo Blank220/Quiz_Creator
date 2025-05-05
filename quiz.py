c78d2b66175edeaff7872c802d337496f7255ae9
Mark Comia <itzextinct2@gmail.com>
2025-05-05 11:01:34 +0800

    adjusted the proper formatting of the question,choices and answer

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
 import random
 
-#open the quiz file
+#open the quiz file and read it
 with open('quiz.txt', 'r') as file:
     lines = file.readlines()
 
@@ -8,8 +8,17 @@ with open('quiz.txt', 'r') as file:
 questions = []
 i = 0
 while i < len(lines):
-
+    if lines[i].startswith('#QUESTIONS'):
+      q = lines[i + 1].strip()
+      a = lines[i + 3].strip()
+      b = lines[i + 4].strip()
+      c = lines[i + 5].strip()
+      d = lines[i + 6].strip()
+      correct = lines[i + 7].strip()[-1]
+      questions.append((q,a,b,c,d))
 #choosing random questions
+#shapol
 #printing the question and choices
 #checks if answer is right
 #tracking of scores
+
